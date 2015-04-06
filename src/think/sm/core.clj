89b736369d0e2330900a0246d449d2007819eb94
Chris Nuernberger <chris@thinktopic.com>
2015-04-06 15:55:40 -0600

    test outline finished, some minimal parsing

--- src/think/sm/core.clj
+++ src/think/sm/core.clj
@@ -1,7 +1,67 @@
-(ns think.sm.core)
+(ns think.sm.core
+  (:require [clojure.xml :as xml])
+  (:import (java.io FileInputStream File)))
+
+(defn is-state-type [type]
+  (case type
+    :state true
+    :final true
+    :parallel true
+    false
+    ))
 
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parse-state [parent node]
+  (let [type (:tag node)
+        id-str (:id (:attrs node))
+        id (if id-str (keyword id-str) nil)
+        new-vestigial-state { :type type :id id :children [] :onentry [] :onexit [] :transitions [] }
+        new-state (reduce parse-state-child new-vestigial-state (:content node))
+        new-children (conj (:children parent) new-state)]
+    (assoc parent :children new-children)))
+
+
+(defn parse-transition [parent node]
+  (let [type :transition
+        target (keyword (:target (:attrs node)))
+        new-vestigial-transition {:type type :onentry [] :onexit [] :target target}
+        new-transition (reduce parse-state-child new-vestigial-transition (:content node))
+        new-transitions (conj (:transitions parent) new-transition)]
+    (assoc parent :transitions new-transition)))
+
+(defn parse-log [node]
+  { :type :log :label (:label (:attrs node)) :expr (:expr (:attrs node)) }) 
+
+(defn parse-executable-content [content node]
+  (let [item (case (:tag node)
+               :log (parse-log node)
+               (throw (Throwable. "Unrecognized executable content")))]
+    (conj content item)))
+                
+        
+ (defn parse-entry-exit [parent node]
+   (let [dest (:tag node)
+         content (reduce parse-executable-content [] (:content node))
+         new-entry (conj (dest parent) content)]
+     (assoc parent dest new-entry)))
+      
+
+(defn parse-state-child [state child]
+  (let [type (:tag child)]
+    (if (is-state-type type)
+      (parse-state state child)
+      (if (= type :transition)
+        (parse-transition state child )
+        (if (or (= type :onentry)
+                (= type :onexit))
+          (parse-entry-exit state child)
+          (throw (Throwable. "Unrecognized state child")))))))
+                   
+
+(defn parse-xml-scxml [node]
+  (reduce parse-state-child {:type :scxml :children [] :datamodel (keyword (:attrs node)) } (:content node)))
+
+(defn load-scxml-file [fname]
+  (let [xml-dom (xml/parse (FileInputStream. fname))]
+    (parse-xml-scxml xml-dom)))
+
--- test/think/sm/core_test.clj
+++ test/think/sm/core_test.clj
@@ -1,7 +1,20 @@
 (ns think.sm.core-test
   (:require [clojure.test :refer :all]
+            [clojure.string :as str]
             [think.sm.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
+  (testing (.concat "testing from: " (System/getProperty "user.dir"))
     (is (= 0 1))))
+
+(def test-base-dir "data")
+
+(defn run-data-test [testname is-optional]
+  (testing (.concate "scxml conformance test: " testname))
+  (let [subdir (if is-optional "optional" "mandatory")
+        full-test-name (str/join [test-base-dir "/" subdir "/test" testname ".txml.ecma.scxml"])
+        machine (load-scxml-file full-test-name)
+        configuration (execute machine)
+        pass-count (count (filter (fn [state] (= (:id state) :pass)) configuration))]
+    (is (= 1 pass-count))))
+    
